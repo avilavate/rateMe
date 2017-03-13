6630bf965d8fe65ea5be5a0a20be6b19dc74077c
avilavate <avilavate123@gmail.com>
2017-03-13 17:36:22 +0530

    Added more routs

--- rateMe/routes/routes.js
+++ rateMe/routes/routes.js
@@ -3,11 +3,14 @@ module.exports = (app) => {
     app.get('/', function (req, res) {
         res.render('index', { 'title': 'RateMe || Index' });
     });
-    app.get('/', function (req, res) {
+    app.get('/index', function (req, res) {
         res.render('index', { 'title': 'RateMe || Index' });
     });
     app.get('/user', function (req, res) {
         res.render('user', { 'title': 'RateMe || user' });
+    });
+    app.get('/signup', function (req, res) {
+        res.render('signup', { 'title': 'RateMe || signup' });
     });
 }
 
